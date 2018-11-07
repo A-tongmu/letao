ecc035d5351c8cfe047deb3448e96b7765014125
A-tongmu <1109428745@qq.com>
2018-11-07 10:43:29 +0800

    购物车完结

--- admin/js/common.js
+++ admin/js/common.js
@@ -6,3 +6,30 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+
+	// 验证登录
+	$.ajax({
+		url:'/api/employee/checkRootLogin',
+		type:'get',
+		success:function(info){
+			if(info.error){
+				alert(info.message);
+				location.href='/letao/admin/login.html?url='+location.href;
+			}
+		}
+	})
+
+
+	// 退出登录
+	$('.fa-sign-out').parent().on('click',function(){
+		$.ajax({
+			url:'/api/employee/employeeLogout',
+			type:'get',
+			success:function(info){
+				if(info.success){
+					location.href='/letao/admin/login.html';
+				}
+			}
+		})
+	})
